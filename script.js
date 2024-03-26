8b68bc9e43dd630f35ec3db26704fd2c8e8bd55d
daniele-rosati <drosati151@gmail.com>
2024-03-26 16:57:58 +0100

    stampa messaggio

---
+++ script.js
@@ -0,0 +1,15 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            message: 'League of legends',
+            // imageSource: "league-of-legends-cover1.webp"
+        };
+    }
+
+    
+  
+
+}).mount('#app');
+ 
